9901e8ea2b0f2ff296d72ca56801c023ddd480b6
Markus Thomsen <matho@itu.dk>
2017-11-27 14:33:30 +0100

    mempool.py

--- mempool.py
+++ mempool.py
@@ -2,28 +2,35 @@ from heapq import heappush, heappop, heapify
 
 class mempool:
     def __init__(self,txs=[]):
-        self.item_set = set([])
+        self.tx_set = set([])
         input_txs = []
         for tx in txs:
             input_txs.append(1./tx.price_of_byte(),tx)
-            self.item_set.add(tx)
+            self.tx_set.add(tx)
 
         self.heap = heapify(input_txs)
 
     def contains(self, item):
-        return item in self.item_set
+        return item in self.tx_set
 
     def push_tx(self, tx):
-        self.item_set.add(tx)
+        self.tx_set.add(tx)
         heappush(self.heap,(1./tx.price_of_byte(),tx))
 
-    def pop_tx(self, tx):
+    def pop_tx(self):
         try:
             tx = heappop(self.heap)
-            self.item_set.remove(tx[1])
+            self.tx_set.remove(tx[1])
             return tx[1]
         except:
             return None
+
+    def remove_items(self, tx_list):
+        for tx in tx_list:
+            if tx in self.tx_set:
+                self.heap.remove((1./tx.price_of_byte(), tx))
+                self.tx_set.remove(tx)
+        heapify(self.heap)
 
     def threshold_pop_tx(self, threshold, tx):
         if(heap[0][1].size() < threshold):
@@ -33,6 +40,6 @@ class mempool:
                 if tx[1].size() < threshold:
                     self.heap.remove(tx)
                     heapify(self.heap)
-                    self.item_set.remove(tx[1])
+                    self.tx_set.remove(tx[1])
                     return tx[1]
             return None
